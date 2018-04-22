d4d32b2aa713e67d96aa2a7c50edc3856b719c70
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2018-04-22 14:38:09 +0200

    Return route expression instead of void

--- src/main/kotlin/com/github/tronghn/webfluxdemo/ApiRoutes.kt
+++ src/main/kotlin/com/github/tronghn/webfluxdemo/ApiRoutes.kt
@@ -9,17 +9,15 @@ import org.springframework.web.reactive.function.server.router
 class ApiRoutes(val apiHandler: ApiHandler) {
 
     @Bean
-    fun apiRouter() {
-        router {
-            ("/api" and accept(APPLICATION_JSON)).nest {
-                "/posts".nest {
-                    GET("/", apiHandler::fetchPosts)
-                    GET("/stream", apiHandler::streamPosts)
-                }
-                "/albums".nest {
-                    GET("/", apiHandler::fetchAlbums)
-                    GET("/stream", apiHandler::streamAlbums)
-                }
+    fun apiRouter() = router {
+        ("/api" and accept(APPLICATION_JSON)).nest {
+            "/posts".nest {
+                GET("/", apiHandler::fetchPosts)
+                GET("/stream", apiHandler::streamPosts)
+            }
+            "/albums".nest {
+                GET("/", apiHandler::fetchAlbums)
+                GET("/stream", apiHandler::streamAlbums)
             }
         }
     }
